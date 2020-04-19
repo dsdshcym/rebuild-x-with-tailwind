7cee0ad826d8b82c02516f487d23186adb87e4a3
Yiming Chen <dsdshcym@gmail.com>
2020-04-19 21:51:06 +0800

    add group-hover variants to backgroundColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   },
   variants: {
     borderColor: ["responsive", "hover", "focus", "focus-within"],
-    backgroundColor: ["responsive", "hover", "focus", "focus-within"],
+    backgroundColor: [
+      "responsive",
+      "hover",
+      "focus",
+      "group-hover",
+      "focus-within",
+    ],
     textDecoration: ["responsive", "hover", "focus", "group-hover"],
   },
   plugins: [],
